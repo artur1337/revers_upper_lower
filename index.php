29e099013ee8ae6eff96e07f7ea02c15e78888c7
artur1337 <artur.pozitiv@gmail.com>
2018-03-19 14:06:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<?php include('revers_upper_lower.php');
+echo reverse("Vardas")."<br>";
+echo upper("vardas")."<br>";
+echo lower("VARDAS")."<br>";
+?>
+</body>
+</html>
---
+++ revers_upper_lower.php
@@ -0,0 +1,15 @@
+<?php
+function reverse($string){
+    $string=strrev($string);
+    return $string;
+}
+
+function upper($string){
+    $string=strtoupper($string);
+    return $string;
+}
+
+function lower($string){
+    $string = strtolower($string);
+    return $string;
+}
